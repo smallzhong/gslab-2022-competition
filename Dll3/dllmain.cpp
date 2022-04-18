9e85cbe3ef38ac15748e46a066623eed3c728136
小钟 <55563027+smallzhong@users.noreply.github.com>
2022-04-18 11:49:51 +0800

    14..不是0x14.....

--- Dll3/dllmain.cpp
+++ Dll3/dllmain.cpp
@@ -72,6 +72,7 @@ EXTERN_C VOID VmxExitHandler(PGuestContext context)
 
 #define ORIGIN_A1_X -0.9
 #define ORIGIN_A1_Y 0.882840
+
 	static int ct = 0;
 	ct++;
 	if (ct <= 6)
@@ -92,6 +93,24 @@ EXTERN_C VOID VmxExitHandler(PGuestContext context)
 		info->a3.x = a1x, info->a3.y = a1y - ygap;
 		info->a4.x = a1x + xgap, info->a4.y = a1y - ygap;
 	}
+	else if (ct == 10)
+	{
+		float a1x = ORIGIN_A1_X + (7 - 6) * (xgap + xbet);
+		float a1y = ORIGIN_A1_Y - (2 - 1) * (ygap + ybet);
+		info->a1.x = a1x, info->a1.y = a1y;
+		info->a2.x = a1x + xgap, info->a2.y = a1y;
+		info->a3.x = a1x, info->a3.y = a1y - ygap;
+		info->a4.x = a1x + xgap, info->a4.y = a1y - ygap;
+	}
+	else if (ct == 11)
+	{
+		float a1x = ORIGIN_A1_X + (8 - 6) * (xgap + xbet);
+		float a1y = ORIGIN_A1_Y - (3- 1) * (ygap + ybet);
+		info->a1.x = a1x, info->a1.y = a1y;
+		info->a2.x = a1x + xgap, info->a2.y = a1y;
+		info->a3.x = a1x, info->a3.y = a1y - ygap;
+		info->a4.x = a1x + xgap, info->a4.y = a1y - ygap;
+	}
 
 	if (ct == 42) ct = 0;
 
@@ -101,22 +120,22 @@ EXTERN_C VOID VmxExitHandler(PGuestContext context)
 	{
 		TT++;
 		if (TT == 12) A("");
-		/*A("a1.x = %f a1.y = %f a2.x = %f a2.y = %f a3.x = %f a3.y = %f a4.x = %f a4.y = %f",
+		A("a1.x = %f a1.y = %f a2.x = %f a2.y = %f a3.x = %f a3.y = %f a4.x = %f a4.y = %f",
 			info->a1.x, info->a1.y,
 			info->a2.x, info->a2.y,
 			info->a3.x, info->a3.y,
-			info->a4.x, info->a4.y);*/
-		A("左上角坐标：%f %f，上下间距：%f 左右间距：%f\n", info->a1.x, info->a1.y, info->a1.y - info->a3.y, info->a2.x - info->a1.x);
+			info->a4.x, info->a4.y);
+		/*A("左上角坐标：%f %f，上下间距：%f 左右间距：%f\n", info->a1.x, info->a1.y, info->a1.y - info->a3.y, info->a2.x - info->a1.x);
 		if (TT != 1)
 		{
 			A("左右间隔：%f 上下间隔：%f", info->a1.x - last_info.a1.x,
 				last_info.a4.y - info->a2.y);
-		}
+		}*/
 		memcpy(&last_info, info, sizeof(Info)); // 备份
 	}
 }
 
-DWORD WINAPI mythread(LPVOID lpParameter)
+__declspec(dllexport) DWORD WINAPI mythread(LPVOID lpParameter)
 {
 	A("in mythread\n");
 	PULONG64 saddr = (PULONG64)0x140008308;
@@ -125,7 +144,7 @@ DWORD WINAPI mythread(LPVOID lpParameter)
 
 	// 在shellcode+30e的地方下hook
 	PUCHAR t = (PUCHAR)((*saddr) + 0x30e);
-	g_origin = (ULONG64)((*saddr) + 0x322); // 需要跳回的地方
+	g_origin = (ULONG64)((*saddr) + 0x31c); // 需要跳回的地方
 
 	char bufcode[] = {
 					0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,
@@ -145,14 +164,14 @@ DWORD WINAPI mythread(LPVOID lpParameter)
 
 	A("AsmVmxExitHandler = %llx\r\n", AsmVmxExitHandler);
 	memcpy(t, bufcode, sizeof bufcode);
-	A("第二次成功！");
+	A("memcpy完成");
 
 	return 0;
 }
 
 
 
-BOOL APIENTRY DllMain(HMODULE hModule,
+__declspec(dllexport) BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
 	LPVOID lpReserved
 )
